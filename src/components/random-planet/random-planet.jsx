ebfde9a553852165b20c24e85b4191542b4128dd
Volodymyr <volodymyr.bushko@gmail.com>
2020-11-20 09:35:51 +0200

    update random-planet and add transformation for swapi-service

--- src/components/random-planet/random-planet.jsx
+++ src/components/random-planet/random-planet.jsx
@@ -1,29 +1,52 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './random-planet.css';
 
-const RandomPlanet = () => {
-  return (
-    <div className="random-planet">
-      <div className="planet-image">
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrPPh9aAZqY9h8-JGKlGd9QJgDal_VlG3y_g&usqp=CAU"
-             alt="planet"/>
-      </div>
-      <div className="planet-info">
-        <h4 className="planet-title">Planet Name</h4>
-        <ul className="planet-info-list">
-          <li className="planet-info-item">
-            Population: 123
-          </li>
-          <li className="planet-info-item">
-              Rotation Period: 321
-          </li>
-          <li className="planet-info-item">
-            Diameter: 100
-          </li>
-        </ul>
-      </div>
-    </div>
-  );
-}
+import SwapiService from '../../services/swapi-service';
 
-export default RandomPlanet;
+export default class RandomPlanet extends Component {
+
+  constructor(props) {
+    super(props);
+    this.swapiService = new SwapiService();
+    this.state = {planet: {}};
+    this.updatePlanet();
+  }
+
+  onPlanetLoaded = planet => {
+    this.setState({planet});
+  }
+
+  updatePlanet() {
+    const id = Math.floor(Math.random() * 25) + 2;
+    this.swapiService
+      .getPlanet(id)
+      .then(this.onPlanetLoaded);
+  }
+
+  render() {
+    const {planet: {id, name, population, rotationPeriod, diameter}} = this.state;
+    return (
+      <div className="random-planet">
+        <div className="planet-image">
+          <img src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
+               alt="planet"/>
+        </div>
+        <div className="planet-info">
+          <h4 className="planet-title">{name}</h4>
+          <ul className="planet-info-list">
+            <li className="planet-info-item">
+              Population: {population}
+            </li>
+            <li className="planet-info-item">
+              Rotation Period: {rotationPeriod}
+            </li>
+            <li className="planet-info-item">
+              Diameter: {diameter}
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+
+}
--- src/services/swapi-service.js
+++ src/services/swapi-service.js
@@ -10,30 +10,72 @@ export default class SwapiService {
   }
 
   async getAllPeople() {
-    const {results} = await this.getResource('/people');
-    return results;
+    const {results: people} = await this.getResource('/people');
+    return people.map(this._transformPerson);
   }
 
-  getPerson(id) {
-    return this.getResource(`/people/${id}`);
+  async getPerson(id) {
+    const person = await this.getResource(`/people/${id}`);
+    return this._transformPerson(person);
   }
 
   async getAllPlanets() {
-    const {results} = await this.getResource('/planets')
-    return results;
+    const {results: planets} = await this.getResource('/planets');
+    return planets.map(this._transformPlanet);
   }
 
-  getPlanet(id) {
-    return this.getResource(`planets/${id}`);
+  async getPlanet(id) {
+    const planet = await this.getResource(`/planets/${id}`);
+    return this._transformPlanet(planet);
   }
 
   async getAllStarships() {
-    const {results} = await this.getResource('/starships')
-    return results;
+    const {results: starships} = await this.getResource('/starships')
+    return starships.map(this._transformStarship);
   }
 
-  getStarships(id) {
-    return this.getResource(`starships/${id}`);
+  async getStarships(id) {
+    const starship = await this.getResource(`/starships/${id}`);
+    return this._transformStarship(starship);
+  }
+
+  _transformPlanet(planet) {
+    return {
+      id: this._getItemId(planet),
+      name: planet.name,
+      population: planet.population,
+      rotationPeriod: planet.rotation_period,
+      diameter: planet.diameter
+    }
+  }
+
+  _transformStarship(starship) {
+    return {
+      id: this._getItemId(starship),
+      name: starship.name,
+      model: starship.model,
+      menufacturer: starship.manufacturer,
+      costInCredits: starship.costInCredits,
+      length: starship.length,
+      crew: starship.crew,
+      passengers: starship.passengers,
+      cargoCapacity: starship.cargoCapacity
+    }
+  }
+
+  _transformPerson(person) {
+    return {
+      id: this._getItemId(person),
+      name: person.name,
+      gender: person.gender,
+      birthYear: person.birthYear,
+      eyeColor: person.eyeColor
+    }
+  }
+
+  _getItemId(item) {
+    const idRegExp = /\/([0-9]*)\/$/;
+    return item.url.match(idRegExp)[1];
   }
 
 }
